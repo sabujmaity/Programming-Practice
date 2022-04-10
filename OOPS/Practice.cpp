72912988cdb4f700ccc4753f1617526470670c0c
CobblingNexus <sabujmaity29@gmail.com>
2022-04-10 20:39:21 +0530

    Second Commit

---
+++ OOPS/Practice.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define fo(i,n) for(i=0;i<n;i++)
+#define pb push_back
+#define F first
+#define S second
+typedef long long ll;
+typedef long double ld;
+
+class student{
+    int rollnumber;
+};
+
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout); 
+    #endif
+
+
+
+    
+    
+
+}

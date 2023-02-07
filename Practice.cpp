5be478650fac0c9823c783bbd31510718304bc75
SABUJ MAITY <sabujmaity29@gmail.com>
2023-02-07 18:31:13 +0530

    USing the C++ Custom Code snippet

--- Practice.cpp
+++ Practice.cpp
@@ -1,16 +1,24 @@
-#include <iostream>
-#include <vector>
-#include <string>
-
+#include<bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+#define fo(i,n) for(i=0;i<n;i++)
+#define pb push_back
+#define F first
+#define S second
+typedef long long ll;
+typedef long double ld;
 
-    for (const string& word : msg)
-    {
-        cout << word << " ";
-    }
-    cout << endl;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout); 
+    #endif
+
+    
+    
+
 }

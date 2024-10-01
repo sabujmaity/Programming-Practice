a90d8fe62ac164cb92f9e4c6f6b7f02f6b9ec950
sabujmaity <sabujmaity29@gmail.com>
2024-10-01 15:05:14 +0530

    Started with CSES Problem set

---
+++ CSES Problem Set/Weird_Algorithm.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define fo(i,n) for(i=0;i<n;i++)
+#define pb push_back
+#define F first
+#define S second
+typedef long long ll;
+typedef long double ld;
+
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout); 
+    #endif
+    int a;
+    cin >> a;
+    cout << a << " ";
+    while(a > 1){
+        if (a % 2 == 0){
+            a = a/2;
+            cout << a << " ";
+        }
+        else{
+            a = a*3 + 1; 
+            cout << a << " ";
+        }
+    }
+
+    
+    
+
+}

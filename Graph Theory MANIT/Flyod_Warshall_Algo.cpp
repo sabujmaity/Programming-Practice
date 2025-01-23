68e3efc8ed68832edffa93a9bf7a7e1d702aec8d
sabujmaity <sabujmaity29@gmail.com>
2025-01-23 11:55:43 +0530

    Flyod Warshall Code added

--- Graph Theory MANIT/Flyod_Warshall_Algo.cpp
+++ Graph Theory MANIT/Flyod_Warshall_Algo.cpp
@@ -10,6 +10,29 @@ using namespace std;
 typedef long long ll;
 typedef long double ld;
 
+void shortest_distance(vector<vector<int>>& grid){
+        int n = grid.size();
+        for(int i = 0; i<n; i++) {
+            for(int j = 0; j<n; j++) {
+                if(grid[i][j] == -1)
+                    grid[i][j] = 100000;
+            }
+        }
+        for(int via = 0; via < n; via++) {
+            for(int i = 0; i<n; i++) {
+                for(int j = 0; j<n; j++) {
+                   grid[i][j] = min(grid[i][j], grid[i][via] + grid[via][j]);
+                }
+            }
+        }
+        for(int i = 0; i<n; i++) {
+            for(int j = 0; j<n; j++) {
+                if(grid[i][j] == 100000)
+                    grid[i][j] = -1;
+            }
+        }
+	}
+
 
 int main(){
     IOS;
@@ -19,7 +42,28 @@ int main(){
     freopen("output.txt", "w", stdout); 
     #endif
 
-    
+    int n;
+    cin >> n;
+    vector<vector<int>> grid(n, vector<int>(n));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> grid[i][j];
+        }
+    }
+    shortest_distance(grid);
+    cout << "The shortest distance matrix is:\n";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (grid[i][j] == -1)
+                cout << "INF ";
+            else
+                cout << grid[i][j] << " ";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+
 
     
     
